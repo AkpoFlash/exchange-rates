4d1fde2739f582e1bc3b015415e2582331d184c1
Kirill Anisimov <akpoflash@gmail.com>
2020-11-09 21:06:46 +0300

    Init accounts data

---
+++ src/accounts/accounts.ts
@@ -0,0 +1,17 @@
+import { Currency } from '../enums/currency';
+import { Account } from '../types/account';
+
+export const USERS_ACCOUNTS: Account = [
+	{
+		currency: Currency.GBP,
+		amount: '1040.5',
+	},
+	{
+		currency: Currency.USD,
+		amount: '550.24',
+	},
+	{
+		currency: Currency.EUR,
+		amount: '230.12',
+	},
+];
